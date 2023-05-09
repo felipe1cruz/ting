981d14218c5532436d54c33710bfaa996ee8d504
felipe-cruz <piradous@gmail.com>
2023-05-09 11:02:08 -0300

    feat: file_process.py function process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    lines = txt_importer(path_file)
+    insert = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(lines),
+        "linhas_do_arquivo": lines
+    }
+    if insert in instance.data:
+        print(f"Arquivo {path_file} já foi processado anteriormente")
+        return
+    instance.enqueue(insert)
+    print(insert, file=sys.stdout)
 
 
 def remove(instance):
