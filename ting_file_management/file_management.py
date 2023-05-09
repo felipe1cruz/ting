fe70f9fe45e7880cf5183be0d9c1b7a952f449bb
felipe-cruz <piradous@gmail.com>
2023-05-09 10:22:55 -0300

    feat: req 02 - file_management.py , function txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        print("Formato inválido", file=sys.stderr)
+        return []
+
+    try:
+        with open(path_file, 'r') as file:
+            lines = file.read().split('\n')
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return []
+
+    return lines
