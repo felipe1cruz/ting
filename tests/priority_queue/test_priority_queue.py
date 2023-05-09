1c578001094392cd1ee33f0a41d5bf8bb626fdd0
felipe-cruz <piradous@gmail.com>
2023-05-09 13:48:46 -0300

    test: test_priority_queue.py sem avaliador

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,23 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    pq = PriorityQueue
+    pq.enqueue({"nome": "arquivo_prioritario1.txt", "qtd_linhas": 2})
+    pq.enqueue({"nome": "arquivo_nao_prioritario.txt", "qtd_linhas": 10})
+    pq.enqueue({"nome": "arquivo_prioritario2.txt", "qtd_linhas": 1})
+    
+    qty_data_inserted = 3
+    assert len(pq) == qty_data_inserted
+    assert pq.search(0)["nome"] == "arquivo_prioritario1.txt"
+    assert pq.search(1)["nome"] == "arquivo_prioritario2.txt"
+    assert pq.search(2)["nome"] == "arquivo_nao_prioritario.txt"
+
+    assert pq.dequeue()["nome"] == "arquivo_prioritario1.txt"
+    assert pq.dequeue()["nome"] == "arquivo_prioritario2.txt"
+    assert pq.dequeue()["nome"] == "arquivo_nao_prioritario.txt"
+
+    number_out_of_len = 10
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        pq.search(number_out_of_len)
