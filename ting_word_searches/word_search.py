d09d82c9a3b7180ae47a1c5ebe55694e7ed4aea6
felipe-cruz <piradous@gmail.com>
2023-05-10 10:05:00 -0300

    feat: req 08 - word_search.py function search_by_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -17,4 +17,21 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    results = []
+
+    for archive in instance.data:
+        ocurrences = []
+        for i, lines in enumerate(archive["linhas_do_arquivo"]):
+            if word.lower() in lines.lower():
+                ocurrences.append({"linha": i + 1,
+                                   "conteudo": lines.strip()
+                                   })
+        if ocurrences:
+            results.append({
+                "palavra": word,
+                "arquivo": archive["nome_do_arquivo"],
+                "ocorrencias": ocurrences
+            })
+
+    return results
+
